47132eca12aa6e75a6585c7c7c011b0aad304a86
jaeik <jilee@autoware.kr>
2025-06-09 19:31:32 +0900

    feat : add Login Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-
+import { Link } from "react-router-dom";
+import "./assets/styles/style.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 function App() {
-
   return (
-    <div className="test">
-
-    <p>aa</p>
-
-    </div>
-  )
+    <Router>
+      <div className="full-bg">
+        <Link  to="/">
+          <p className="tracking-in-expand title-text">TODO-SCHEDULER</p>
+        </Link>
+      </div>
+      <Routes><Route path="/" element={<Login/>}></Route></Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
