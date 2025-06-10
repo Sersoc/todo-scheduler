2ec6f9b80b44ef1a8acd6b7c334a09cc4498751c
Sersoc <45021157+Sersoc@users.noreply.github.com>
2025-06-10 22:51:40 +0900

    feat : add login logic in front

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,26 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+interface LoginResponse {
+  token: string; // 서버에서 반환하는 JWT 토큰의 필드
+}
 export default function Login() {
   const [userId, setUserId] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const navigate = useNavigate();
-  const handleLogin = async () => {
-    const response = await fetch(``);
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    const response = await fetch(`http://localhost:5000/login`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+        userPassword: userPassword,
+      }),
+    });
 
-    
+    event.preventDefault();
+
+    const data: LoginResponse = await response.json();
+    console.log(`data token ${data.token}`);
+    localStorage.setItem("authToken", data.token);
+    navigate("/"); // 홈 페이지로 이동
+    window.location.reload();
   };
 
-  const handlerPage = () =>{
+  const handlerPage = () => {
     navigate("/signin");
-    
-  }
+  };
   return (
     <div className="login">
       <form className="login-box" onSubmit={handleLogin}>
         <h1>Login</h1>
         <section className="input-box">
-            <h2>ID</h2>
+          <h2>ID</h2>
           <input
             type="text"
             id="username"
@@ -32,7 +47,7 @@ export default function Login() {
           />
         </section>
         <section className="input-box">
-            <h2>PW</h2>
+          <h2>PW</h2>
           <input
             type="password"
             id="userpassword"
@@ -44,9 +59,10 @@ export default function Login() {
           />
         </section>
         <button type="submit">Login</button>
-      <button type="button" onClick={handlerPage}>Sign In</button>  
+        <button type="button" onClick={handlerPage}>
+          Sign In
+        </button>
       </form>
-      
     </div>
   );
 }
